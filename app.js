cf45b2030798d73fed56689542dffe8cb94fa723
y-ikezawa <y.ikezawa93@gmail.com>
2020-08-08 17:12:57 +0900

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 'use strict';
-const number = process.argv[2] || 0;
-let sum = 0;
-for (let i = 0; i <= number; i++){
-  sum += i;
+function fb(n){
+  if(n === 0){
+    return 0;
+  } else if (n === 1){
+    return 1;
+  }
+  return fb(n - 2) + fb(n - 1);
 }
-console.log(sum);
+
+const num = 40;
+for (let i = 0; i <= num; i++){
+  console.log(fb(i))
+}
